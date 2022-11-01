42a8a0e2ee915d46bbe852551510980d3607cd22
akhror-valiev <ahror.valiev@gmail.com>
2022-11-01 15:04:07 +0500

    Set linters for html css and javascript

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import _ from 'lodash'
+import _ from 'lodash';
+
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
+  return element;
 }
 
 document.body.appendChild(component());
