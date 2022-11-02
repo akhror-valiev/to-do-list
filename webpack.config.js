bd5e8585c144079ef5d873b8ce5c869d4ba48723
akhror-valiev <ahror.valiev@gmail.com>
2022-11-03 00:34:35 +0500

    Update the index.html and style.css

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,25 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
   mode: 'development',
 };
